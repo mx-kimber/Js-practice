201795c356386145c3f79dda145a8b0acf5c5d30
Kimber Ogden <mx.kimber@gmail.com>
2023-05-08 16:58:23 -0500

    added more problems and solutions to practice

---
+++ arrays_solved.js
@@ -0,0 +1,61 @@
+// // 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+// var words = ["guitar", "piano", "flute"];
+// words.push("drums");
+// words.push("cowbell");
+// console.log(words);
+
+// // 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+// var letters = ["a", "r", "w", "p"];
+// letters[1] = 34;
+// console.log(letters);
+
+// // 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+// var numbers = [3, 2, 1, 5, 3];
+// var index = 0;
+// while (index < numbers.length) {
+//   console.log(numbers[index]);
+//   index = index + 1;
+// }
+
+// // 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+// var numbers = [3];
+// numbers.push(6);
+// numbers.push(92);
+// numbers.push(8);
+// console.log(numbers);
+
+// // 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+// var strings = ["hello", "hello", "hello"];
+// strings[2] = strings[2].toUpperCase();
+// console.log(strings);
+
+// // 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+// var names = ["erika", "sal", "edgar"];
+// var index = 0;
+// while (index < names.length) {
+//   console.log(names[index]);
+//   index = index + 1;
+// }
+
+// // 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+// var strings = ["black", "white"];
+// strings.push("gray");
+// console.log(strings);
+
+// // 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+// var numbers = [3, 4, 24, 3, 1];
+// numbers[0] = numbers[0] * 10;
+// console.log(numbers);
+
+// // 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+// var numbers = [93, 23];
+// var index = 0;
+// while (index < numbers.length) {
+//   console.log(numbers[index]);
+//   index = index + 1;
+// }
+
+// // 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+// var countries = ["France", "Korea", "Brazil"];
+// countries.push("Mexico");
+// console.log(countries);
---
+++ methods_solved.js
@@ -0,0 +1,48 @@
+// // 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+// var word = window.prompt("Enter a word: ");
+// console.log(word.toUpperCase());
+
+// // 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+// var number = window.prompt("Enter a number: ");
+// if (parseInt(number) > 100) {
+//   console.log("That's a big number");
+// }
+
+// // 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+// var number1 = window.prompt("Enter a number: ");
+// var number2 = window.prompt("Enter another number: ");
+// console.log(parseInt(number1) + parseInt(number2));
+
+// // 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+// var word = window.prompt("Enter a word: ");
+// console.log(word.split("").reverse().join(""));
+
+// // 5. Write a program that asks the user to enter a number, then prints the number times 10.
+// var number = window.prompt("Enter a number: ");
+// console.log(parseInt(number) * 10);
+
+// // 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+// var word1 = window.prompt("Enter a word: ");
+// var word2 = window.prompt("Enter another word: ");
+// console.log(word1.toUpperCase() + " " + word2.toUpperCase());
+
+// // 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+// var word = window.prompt("Enter a word: ");
+// console.log(word.length);
+
+// // 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+// var number = window.prompt("Enter a number: ");
+// if (parseInt(number) < 0) {
+//   console.log("That's a negative number");
+// }
+
+// // 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+// var number1 = window.prompt("Enter a number: ");
+// var number2 = window.prompt("Enter another number: ");
+// console.log(parseInt(number1) * parseInt(number2));
+
+// // 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+// var word = window.prompt("Enter a word: ");
+// if (word.length > 5) {
+//   console.log("That's a long word");
+// }
