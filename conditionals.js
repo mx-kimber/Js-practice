a29b643cb47df0f0114f7916368d5f7afc4e844d
Kimber Ogden <mx.kimber@gmail.com>
2023-05-08 15:44:34 -0500

    javascript practice - conditionals

--- conditionals.js
+++ conditionals.js
@@ -40,9 +40,27 @@
 
 // # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints - 1 otherwise(only one print statement should occur).
 
+// var number = 20;
+// if (number < 10) {
+//   console.log(9);
+// } else if (number < 20) {
+//   console.log(19);
+// } else if (number < 30) {
+//   console.log(29);
+// } else {
+//   console.log(-1);
+// }
 
 
 // # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints - 100 otherwise.
+
+// var number = 10;
+// var number2 = 11;
+// if (number > 10 || number2 > 10) {
+//   console.log(100);
+// } else {
+//   console.log(-100);
+// }
 
 // # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
